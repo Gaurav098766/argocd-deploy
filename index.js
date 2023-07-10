20838180e7d394ab76e59f6b8af9c7981dd08623
Gaurav Sharma <97042529+Gaurav098766@users.noreply.github.com>
2023-07-10 15:47:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port=5000
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello!! this is running')
+    res.send('Hello!! this is running successfully!!!!!!!!!!!!!!')
 })
 
 app.listen(port,()=>{
